520edf50c7e07b86aba153eef1116d588b913f41
Teodor Raul Chicinas <45625206+teodorraul@users.noreply.github.com>
2024-09-17 22:28:18 +0300

    Update typography.css.ts

--- src/brand/typography.css.ts
+++ src/brand/typography.css.ts
@@ -2,7 +2,7 @@ import { fontFace } from '@vanilla-extract/css';
 
 export const Playfair = fontFace([
 	{
-		src: `url('/fonts/Playfair.woff2') format('woff2')`,
+		src: `url('${process.env.ROOT_PATH}/fonts/Playfair.woff2') format('woff2')`,
 		fontWeight: 'bold',
 		fontStyle: 'normal',
 		fontDisplay: 'swap',
@@ -11,13 +11,13 @@ export const Playfair = fontFace([
 
 export const Inter = fontFace([
 	{
-		src: `url('/fonts/Inter-Medium.woff2') format('woff2')`,
+		src: `url('${process.env.ROOT_PATH}/fonts/Inter-Medium.woff2') format('woff2')`,
 		fontWeight: 'regular',
 		fontStyle: 'normal',
 		fontDisplay: 'swap',
 	},
 	{
-		src: `url('/fonts/Inter-Bold.woff2') format('woff2')`,
+		src: `url('${process.env.ROOT_PATH}/fonts/Inter-Bold.woff2') format('woff2')`,
 		fontWeight: 'bold',
 		fontStyle: 'normal',
 		fontDisplay: 'swap',
